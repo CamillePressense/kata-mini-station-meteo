ba102694e0952176987811e8710ab4db1d4b6f34
Pressense Fasani <camillefasani@gmail.com>
2025-03-28 10:19:12 +0100

    Etape 2: récupérer les coordonnées de la ville

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// A toi de jouer pour cette partie :-) Happy coding !
+const CITYINPUT =  document.querySelector("#cityInput");
+const CITYNAME = document.querySelector("#city");
+const COORDINATES = document.querySelector("#gps");
+const BUTTON = document.querySelector("button");
+
+BUTTON.addEventListener("click", fetchCoordinates);
+
+// fetchCoordinates function calls openstreetmap API to get, on click, coordinates of the city entered in the input.
+
+async function fetchCoordinates(){
+    const city = CITYINPUT.value
+    CITYNAME.textContent = city
+
+    let promise = await fetch(`https://nominatim.openstreetmap.org/search?city=${city}&format=json&addressdetails=1&limit=1`);
+    let response = await promise.json();
+    const {lat, lon} = response[0];
+    //console.log(response[0])
+    COORDINATES.textContent = `Coordonnées GPS: ${lat}, ${lon} `;
+    }
+
+fetchCoordinates()
